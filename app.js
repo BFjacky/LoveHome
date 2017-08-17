955be36bb8494b71f0685ea8220e55dcaf2123a3
chenyunfei <438376555@qq.com>
2017-08-17 20:42:30 +0800

    首页完成了(仅仅手机端)

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express();
+const path = require('path')
+const fs = require('fs')
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.get('/', function (req, res) {
+    fs.readFile(path.join(__dirname, 'public', 'html', 'index.html'), (err, data) => {
+        if (err) {
+            throw err
+        }
+        else {
+            res.setHeader('Content-Type', 'text/html')
+            res.send(data);
+            console.log('返回html')
+        }
+    })
+})
+
+app.listen(80, () => {
+    console.log('running...')
+})
